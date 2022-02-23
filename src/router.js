09795eceb0e0e8022d7d178c3c6ae05e8c6f8d01
BudhikaN <dlbnprabath@gmail.com>
2022-02-23 20:07:36 +0530

    added main layout

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
@@ -14,8 +14,8 @@ const router = createRouter({
         { path: '/', redirect: '/coaches' },
         { path: '/coaches', component: CoachesList },
         {
-            path: '/coaches/:id', component: CoachDetail, children: [
-                { path: '/contact', component: ContactCoach }, // /coaches/c1/contact
+            path: '/coaches/:id', component: CoachDetails, children: [
+                { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
             ]
         },
         { path: '/register', component: CoachRegistration },
